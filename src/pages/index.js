57b0325238e66c9548208c9c85a7c3e31eb7b746
wuubi <56210944+wuubi@users.noreply.github.com>
2021-01-30 23:33:41 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ function Index() {
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
-  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/80104122&color=%23765264&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/theonlymn" title="theonlymn" target="_blank" style="color: #cccccc; text-decoration: none;">theonlymn</a> · <a href="https://soundcloud.com/theonlymn/blue-to-black" title="Blue to Black" target="_blank" style="color: #cccccc; text-decoration: none;">Blue to Black</a></div>
+  <iframe src="https://open.spotify.com/embed/playlist/0VBIQ6M3ryZmUiCx6u8rs6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </main>
   );
 }
