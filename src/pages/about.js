e357be0512b53ad12115f55561f022169ddb6dbc
Kyle OBrien <obrienk@webbhost.net>
2021-02-18 01:39:48 -0600

    Update about.js
    replace with original from index.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,44 @@
-<iframe src="https://open.spotify.com/embed/track/1ucm3MpSCBzowvug8ABnMe" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import '../styles/index.css';
+
+function Index() {
+  const [date, setDate] = useState(null);
+  useEffect(() => {
+    async function getDate() {
+      const res = await fetch('/api/date');
+      const newDate = await res.text();
+      setDate(newDate);
+    }
+    getDate();
+  }, []);
+  return (
+    <main>
+      <Helmet>
+        <title>Gatsby + Node.js (TypeScript) API</title>
+      </Helmet>
+      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h2>
+        Deployed with{' '}
+        <a
+          href="https://vercel.com/docs"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Vercel
+        </a>
+        !
+      </h2>
+      <p>
+       a secret by <a href="https://orphic.xyz">Orphic Inc</a>, coming soon
+      </p>
+      <br />
+      <h2>The date according to Node.js (TypeScript) is:</h2>
+      <p>{date ? date : 'Loading date...'}</p>
+  <iframe src="https://open.spotify.com/embed/track/1ucm3MpSCBzowvug8ABnMe" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </main>
+  );
+}
+
+export default Index;
+
