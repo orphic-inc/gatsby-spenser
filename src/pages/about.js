0468286e781fadcdd55cc414a434e43848133a18
Kyle OBrien <obrienk@webbhost.net>
2021-02-18 01:36:10 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,1 @@
-document.write("Hello world!")
+<iframe src="https://open.spotify.com/embed/track/1ucm3MpSCBzowvug8ABnMe" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
